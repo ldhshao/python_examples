2a2002087f53f2b7fbaa810b67d945dba3caa176
ldhshao <liu198456@126.com>
2019-12-07 10:59:33 +0800

    traker: add ember-bootrap         add navbar

--- nodejstest/traker/ember-cli-build.js
+++ nodejstest/traker/ember-cli-build.js
@@ -3,13 +3,11 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let bootstrapPath = 'bower_components/bootstrap-sass/assets/'
   let app = new EmberApp(defaults, {
     // Add options here
-    sassOptions: {
-      includePaths: [
-        bootstrapPath + 'stylesheets'
-      ]
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapCSS': false
     }
   });
 
@@ -25,7 +23,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import(bootstrapPath + 'javascripts/bootstrap.js');
 
   return app.toTree();
 };
