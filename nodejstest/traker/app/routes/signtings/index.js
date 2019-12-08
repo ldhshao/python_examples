8067ef6e3617fc39f5f7393fb6e73a6a75c208f7
ldhshao <liu198456@126.com>
2019-12-08 22:51:52 +0800

    traker: add signtings model data and show them         index -->  signtings

--- nodejstest/traker/app/routes/signtings/index.js
+++ nodejstest/traker/app/routes/signtings/index.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    return [
+      {id:1, location:'tianjin', signtedAt: new Date('2019-03-07')},
+      {id:2, location:'beijing', signtedAt: new Date('2019-03-08')},
+      {id:3, location:'henan', signtedAt: new Date('2019-03-09')},
+      {id:4, location:'hunan', signtedAt: new Date('2019-03-10')}
+    ]
+  }
 });
