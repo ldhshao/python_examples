b0d67dce2e6d35e9e45fa9e6faf4f42c6e847e65
ldhshao <liu198456@126.com>
2019-12-09 23:17:59 +0800

    tracker: add fullName for witness(not test)

--- nodejstest/traker/app/models/witness.js
+++ nodejstest/traker/app/models/witness.js
@@ -1,8 +1,12 @@
 import Model, {attr, hasMany} from '@ember-data/model';
+import Ember from '@ember'
 
 export default Model.extend({
   fName: attr('string'),
   lName: attr('string'),
   email: attr('string'),
-  signtings: hasMany('signting')
+  signtings: hasMany('signting'),
+  fullName: Ember.computed('fName', 'lName', function(){
+    return this.get('fName') + ' ' + this.get('lName');
+  })
 });
--- nodejstest/traker/app/routes/witnesses.js
+++ nodejstest/traker/app/routes/witnesses.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    let w1 = this.store.createRecord('witness', {fName:'donghao', lName:'liu', email:'liu198456@126.com'});
+    return [w1];
+  }
 });
